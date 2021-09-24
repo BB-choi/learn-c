e1602d2e23323555df69b07b0724b5d91fc5636a
BB <choibobae0809@gmail.com>
2021-09-25 08:26:17 +0900

    2.5 변수를 선언하는 방법

---
+++ TBC/lecture/lecture2/main.c
@@ -0,0 +1,22 @@
+int main()
+{
+    // int x; // declaration
+    // int y;
+    // int z;
+
+    // int x, y, z;
+
+    // x = 1; // assignment
+    // y = 2;
+
+    // int x = 1, y= 2, z;
+    // int x = 1, y= 2, z = x + y; 
+    // 초기화와 연산이 함께 있기 때문에 쪼개는 것이 좋다.
+
+    int x = 1, y = 2;
+    int z;
+
+    z = x + y;
+
+    return 0;
+}
