37937a7cf73a226e8f4f163b45aad2bfffff8913
BB <choibobae0809@gmail.com>
2021-09-26 07:03:20 +0900

    2.8 키워드와 예약어

--- TBC/lecture/lecture2/main.c
+++ TBC/lecture/lecture2/main.c
@@ -5,23 +5,20 @@
 // 주석을 달아야 할 곳에는 달고, 달지 말아야 할 곳에는 달지 말기!
 
 #include <stdio.h>
+#include <stdbool.h>
+// https://en.cppreference.com/w/c/keyword
 
 int main()
 {
-    /*
-        This is my code
-    */
+    // int if;
+    // int for;
+    // int printf = 1;
 
-    // variables (x)
-    int x, y, sum;
+    // printf("Hello World!"); // printf is not a function
 
-    x = 1;
-    y = 4;
+    // int _Bool;
+    int bool = 2;
 
-    sum = x + y; // add two numbers (x) --> 변수명을 sum으로 바꾸어서 사용
-
-    printf("\n%i + %i = %i", x, y, sum);
-    printf("\a");
 
     return 0;
 }
