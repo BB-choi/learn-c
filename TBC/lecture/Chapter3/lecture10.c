6dddcfd762f572e2d6c64a5ff020c309c705fe26
BB <choibobae0809@gmail.com>
2021-09-27 15:45:31 +0900

    3.10 문자형

---
+++ TBC/lecture/Chapter3/lecture10.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    char c = 'A';
+    // char d = 65; // d = 'A'
+    char d = '*';
+
+    /* printf("%c %hhd\n", c, c);
+    printf("%c %hhd\n", d, d); */
+
+    // printf("%c \n", c + 1);
+
+    // char a = '\a';
+    // printf("%c", a);
+
+    // printf("\07"); // 8진수 7 //\7
+    // printf("\x7"); // 10진수 7 //\7
+    // printf("\x23"); // #
+
+    // float salary;
+    // // printf("$______");
+    // printf("$______\b\b\b\b\b\b");
+    // scanf("%f", &salary);
+
+    /* printf("AB\tCDEF\n");
+    printf("ABC\tDEF\n"); */
+
+    printf("\\ \'HA+\' \"Hello\" \?\n");
+
+    return 0;
+}
